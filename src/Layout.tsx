ef128a0d59648b32a976c464b9a492372796614d
Helder Oliveira <heldrida@gmail.com>
2021-03-25 11:01:29 +0000

    refactor: 💡 provides sidebar control, switch state

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,25 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect }  from 'react'
 
-interface Props {
+interface LayoutProps {
   sidebar: React.ReactNode,
   content: React.ReactNode,
 }
 
-export const Layout: React.FC<Props> = ({ sidebar, content }) => {
+export const Layout: React.FC<LayoutProps> = ({ sidebar, content }) => {
+  const [transition, setTransition] = useState<boolean>(false)
+  const [menuOpen, setMenuOpen] = useState<boolean>(false)
+
+  const menuToggle = () => setMenuOpen(!menuOpen)
+
+  useEffect(() => {
+    function onWindowResize(e: UIEvent) {
+      const el = e.target as Window
+      if (!transition && el.innerWidth < 768) {
+        setTransition(true)
+      } else if (transition && el.innerWidth >= 768) {
+        setTransition(false)
+      }
+    }
+    window.addEventListener("resize", onWindowResize)
+    return () => window.removeEventListener("resize", onWindowResize);
+  }, [transition]);
+
   return (
     <div className="w-full">
       <div className="bg-gray-200 w-full z-50 flex justify-center items-center h-10 md:h-12 fixed md:relative md:sticky md:top-0">
-        <div className="absolute inset-y-0 left-0 w-16 h-16 md:hidden">
+        <div className="absolute inset-y-0 left-0 w-16 h-16 md:hidden" onClick={menuToggle}>
           menu
         </div>
         Logo
       </div>
       <div className="w-full max-w-screen-2xl mx-auto pt-10">
         <div className="md:flex">
-          <div className="z-20 bg-white fixed transform md:shadow-none md:-translate-x-0 flex-none h-full w-3/4 md:sticky md:top-0 md:w-80">
+          <div className={`z-20 ${transition ? 'transition-transform' : 'transition-none'} duration-300 md:transition-none bg-white fixed transform ${menuOpen ? 'translate-x-1/1 shadow-lg' : '-translate-x-full' } md:shadow-none md:-translate-x-0 flex-none h-full w-3/4 md:sticky md:top-0 md:w-80`}>
             {sidebar}
           </div>
           <div className="z-10 min-w-0 w-full flex-none md:flex-auto max-h-full">
+            <div className={`md:hidden fixed top-0 transition-opacity duration-300 z-10 w-full h-full pointer-events-none bg-black ${menuOpen ? 'bg-opacity-40 pointer-events-auto' : 'bg-opacity-0' }`} onClick={() => { setMenuOpen(false) }} />
             {content}
           </div>
         </div>
