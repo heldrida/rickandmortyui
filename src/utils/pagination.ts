9a35a62b592917d9be8b4894fe9373b7ee5e010f
Helder Oliveira <heldrida@gmail.com>
2021-03-25 17:20:59 +0000

    feat: 🎸 created Paginator util

---
+++ src/utils/pagination.ts
@@ -0,0 +1,38 @@
+interface Pagination {
+  total: number,
+  range: number,
+  currentIndex: number,
+}
+export const paginator: (pagination: Pagination) => number[] = ({
+  total,
+  range,
+  currentIndex,
+}) => {
+  let numbered = [];
+  const last = currentIndex + range
+
+  // Validate
+  if (currentIndex >= total || range < 1) {
+    throw new Error("Paginator failure: Unexpected input!");
+  }
+
+  // Mutate range when exceeds total
+  if (last > total) {
+    range = total - currentIndex
+  }
+
+  // Spread values to range
+  for (let i = 0, x = currentIndex; i < range; i++, x++) {
+    numbered.push(x)
+  }
+
+  // Show dots when too long (represented by -1)
+  if (last < total) {
+    numbered.push(-1)
+  }
+
+  // Last page is always available
+  numbered.push(total)
+
+  return numbered;
+}
