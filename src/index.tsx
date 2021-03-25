f6bde7286da3d5eb24eed3df31a1ef27b47a1904
Helder Oliveira <heldrida@gmail.com>
2021-03-25 09:08:05 +0000

    style: 💄 tailwind css, hello world

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom'
 ReactDOM.render(
   // Strict mode checks are run in development mode only
   <React.StrictMode>
-    <p>{'Hello world!'}</p>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+      {'Hello tailwindcss'}
+    </button>
   </React.StrictMode>,
   document.getElementById('root')
 )
