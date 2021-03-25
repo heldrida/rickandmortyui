d62a0e28541ec87cf24cf6b544c401d91690cb2f
Helder Oliveira <heldrida@gmail.com>
2021-03-25 10:24:35 +0000

    style: 💄 update Card look, use props (name, status, etc)

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,3 +1,40 @@
 import React from 'react'
 
-export const Card = () => <img className="w-full" src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Avatar" />
+interface Props {
+  name: string,
+  specie: string,
+  status: string,
+}
+
+export const Card: React.FC<Props> = ({ name, specie, status }) => {
+  const onDetails = () => {
+    console.warn("TODO: show detail panel")
+  }
+  
+  return (
+    <>
+      <div className="rounded overflow-hidden shadow-lg">
+        <img className="w-full" src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Avatar" />
+        <div className="px-6 py-4 pb-0">
+          <p className="font-bold text-gray-700 text-base">
+            {name}        
+          </p>
+          <p className="text-gray-700 text-base">
+            {specie}
+          </p>
+          <p className="text-gray-700 text-base">
+            {status}
+          </p>
+        </div>
+        <div className="text-center p-6">
+          <button
+            className="bg-green-400 hover:bg-green-300 text-white font-bold py-2 px-4 rounded"
+            onClick={onDetails}
+          >
+            Details
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
