8dcccc1bf61f24a68a5de4c10e461ce870ed653a
Helder Oliveira <heldrida@gmail.com>
2021-03-25 10:08:27 +0000

    refactor: 💡 separate input filter by name and use props

--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 
+interface InputFilterProps {
+  placeholder: string,
+}
+
+const InputFilter: React.FC<InputFilterProps> = ({ placeholder }) => (
+  <input
+    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+    type="text"
+    placeholder={placeholder} />
+)
+
 export const Sidebar = () => {
   return (
     <div className="px-4">
       <div className="mb-8">
-        <input
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        type="text"
-        placeholder="Filter by name" />
+        <InputFilter placeholder="Filter by name" />
       </div>
     </div>
   )
