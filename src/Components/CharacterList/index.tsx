1261f4cb4d6d14d75761c58c71f10d788c4f66b9
Helder Oliveira <heldrida@gmail.com>
2021-03-25 23:41:46 +0000

    style: 💄 show loading animation on pending characters request

--- src/Components/CharacterList/index.tsx
+++ src/Components/CharacterList/index.tsx
@@ -4,6 +4,7 @@ import { Pagination } from '../Pagination'
 import { useAppDispatch, useAppSelector } from '../../redux/hooks'
 import { fetchCharacters } from '../../redux/slices/characterSlice'
 import { Character } from '../../redux/slices/characterSlice'
+import loaderImg from '../../images/loader.svg';
 
 export const CharacterList = () => {
   const {
@@ -19,9 +20,9 @@ export const CharacterList = () => {
   }, [])
 
   useEffect(() => {
-    const { info, results, error } = characterResults;
+    const { results, error } = characterResults;
 
-    console.log(results)
+    console.log(characterResults)
 
     // On error show API error in console
     if (error) {
@@ -32,25 +33,31 @@ export const CharacterList = () => {
     if (Array.isArray(results)) {
       setList(results)
     }
-
-    // Update pagination
-    // and the loading status
-    console.log("info", info)
   }, [characterResults])
 
   return (
     <>
-      <div className="w-full px-4 m:px-0 grid gap-4 grid-cols-1 lg:grid-cols-3">
-        {
-          list.map((props, idx) => <Card key={idx} {...props} />)
-        }
-      </div>
-        {
-          characterResults.info?.count &&
-          <div className="w-full pt-5 pb-5 pr-1">
-            <Pagination total={characterResults.info?.count} range={5} />
+      {
+        characterResults.loading &&
+        <div className="flex w-full h-full justify-center align-center">
+          <img className="animate-spin h-20 w-20 opacity-10" src={loaderImg} />
+        </div> ||
+        <>
+          <div className="w-full px-4 m:px-0 grid gap-4 grid-cols-1 lg:grid-cols-3">
+            {
+              list.map((props, idx) => <Card key={idx} {...props} />)
+            }
           </div>
-        }
+          <>
+            {
+              characterResults.info?.count &&
+              <div className="w-full pt-5 pb-5 pr-1">
+                <Pagination total={characterResults.info?.count} range={5} />
+              </div>
+            }
+          </>
+        </>
+      }
     </>
   )
 }
