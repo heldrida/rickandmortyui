b0f70e7fd451558cf010b996b96b5994806cabc6
Helder Oliveira <heldrida@gmail.com>
2021-03-25 17:21:16 +0000

    test: 💍 created Pagination unit tests

---
+++ src/utils/pagination.test.ts
@@ -0,0 +1,46 @@
+import { paginator } from './pagination';
+
+describe('Paginator is valid', () => {
+  test('when the current index is 1, in a total of 100', () => {
+    const expected = [1, 2, 3, 4, 5, -1, 100]
+    const result = paginator({
+      total: 100,
+      range: 5,
+      currentIndex: 1,
+    })
+    expect(expected).toEqual(result);
+  });
+
+  test('when the current index is 98, in a total of 100', () => {
+    const expected = [98, 99, 100]
+    const result = paginator({
+      total: 100,
+      range: 5,
+      currentIndex: 98,
+    })
+    expect(expected).toEqual(result);
+  });
+
+  test('when the current index is 197, in a total of 571', () => {
+    const expected = [197, 198, 199, -1, 571]
+    const result = paginator({
+      total: 571,
+      range: 3,
+      currentIndex: 197,
+    })
+    expect(expected).toEqual(result);
+  });
+})
+
+describe("Paginator throws", () => {
+  test('throws unexpected error', () => {
+    const cb = () =>
+      paginator({
+        total: 1,
+        range: 3,
+        currentIndex: 197,
+      })
+
+    expect(cb).toThrow('Paginator failure: Unexpected input!')
+  });
+})
