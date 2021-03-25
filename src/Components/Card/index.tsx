1de7ba363121789f0d68a0379897b01e1fc93c5f
Helder Oliveira <heldrida@gmail.com>
2021-03-25 09:58:43 +0000

    feat: 🎸 add Card component placeholder

---
+++ src/Components/Card/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export const Card = () => <img className="w-full" src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Avatar" />
--- src/Components/CharacterList/index.tsx
+++ src/Components/CharacterList/index.tsx
@@ -1,3 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Card } from "../Card";
 
-export const CharacterList = () => <><p>{'CharacterListComponent'}</p></>
+export const CharacterList = () => {
+  return (
+    <div className="px-4 m:px-0 grid gap-4 grid-cols-1 lg:grid-cols-3">
+      {
+        [...Array.from({ length: 12 })].map(_ => <Card />)
+      }
+    </div>
+  )
+}
