e3b1dede9dda5e23868077be18fe12bea2faa775
Helder Oliveira <heldrida@gmail.com>
2021-03-25 15:46:51 +0000

    feat: 🎸 Episodes component, tab handler and mock data

--- src/Components/Details/index.tsx
+++ src/Components/Details/index.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from '../Button';
 import { useDisplayState } from '../../Context/Display';
+import { Episodes } from '../Episodes';
 
 interface DetailsProps {
   goBackHandler: () => void
@@ -15,28 +16,35 @@ export const Details: React.FC<DetailsProps> = ({ goBackHandler }) => {
   }, [display])
 
   return (
-    <div className="md:flex w-full pt-20 px-4 md:pt-0 mx-auto max-w-screen-lg">
-      <div className="md:w-1/6">
-        <Button placeholder="Go back" callback={goBackHandler} />
+    <>
+      <div className="md:flex w-full pt-20 px-4 md:pt-0 mx-auto max-w-screen-lg">
+        <div className="md:w-1/6">
+          <Button placeholder="Go back" callback={goBackHandler} />
+        </div>
+        <div className="md:w-5/6">
+          {
+            display?.character &&
+            <>
+              <div className="md:flex pt-10">
+                <div className="md:w-1/3">
+                  <img src={display.character.image} alt={display.character.name} />
+                </div>
+                <div className="md:w-2/3 pt-5 md:pt-0 md:pl-10">
+                  {
+                    display?.character &&
+                    Object.keys(display.character)
+                      .filter(key => key !== "image")
+                      .map(key => display.character && <p>{display.character[key]}</p>)
+                  }
+                </div>
+              </div>
+              <div className="w-full pt-10">
+                <Episodes />
+              </div>
+            </>
+          }
+        </div>
       </div>
-      <div className="md:w-5/6">
-        {
-          display?.character &&
-          <div className="md:flex pt-10">
-            <div className="md:w-1/3">
-              <img src={display.character.image} alt={display.character.name} />
-            </div>
-            <div className="md:w-2/3 pt-5 md:pt-0 md:pl-10">
-              {
-                display?.character &&
-                Object.keys(display.character)
-                  .filter(key => key !== "image")
-                  .map(key => display.character && <p>{display.character[key]}</p>)
-              }
-            </div>
-          </div>
-        }
-      </div>
-    </div>
+    </>
   )
 }
