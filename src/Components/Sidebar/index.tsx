a1a2f4e6ece3dddaafa739bdfb58a6be0262e13f
Helder Oliveira <heldrida@gmail.com>
2021-03-25 10:01:27 +0000

    feat: 🎸 filter by name placeholder

--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -1,3 +1,14 @@
 import React from 'react'
 
-export const Sidebar = () => <><p>{'SidebarComponent'}</p></>
+export const Sidebar = () => {
+  return (
+    <div className="px-4">
+      <div className="mb-8">
+        <input
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        type="text"
+        placeholder="Filter by name" />
+      </div>
+    </div>
+  )
+}
