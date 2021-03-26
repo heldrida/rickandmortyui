3cf4ce1084d7961fb29cf0c37dac5e5c3b2ffa8c
Helder Oliveira <heldrida@gmail.com>
2021-03-26 20:53:50 +0000

    perf: ⚡️ smooth the circle CTA scroll to the top

--- src/Components/ButtonCircleUp/index.tsx
+++ src/Components/ButtonCircleUp/index.tsx
@@ -22,7 +22,7 @@ export const ButtonCircleUp: React.FC<{ menuOpen: boolean }> = ({menuOpen}) => {
 
   return (
     <div
-      onClick={() => window.scrollTo(0, 0)}
+      onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}
       className={`${!menuOpen && show ? 'hover:scale-110 hover:opacity-80' : 'pointer-events-none opacity-0'} fixed ${atBottom ? '-translate-y-20' : '' } cursor-pointer translate-opacity translate-y-0 bottom-10 transform transition-all duration-300 right-5 md:right-10 shadow-md z-20 font-bold text-gray-700 rounded-full bg-yellow-200 flex items-center justify-center`}
       style={{ width: '4rem', height: '4rem', fontSize: '2rem' }}>
       <span className="pb-2">▲</span>
