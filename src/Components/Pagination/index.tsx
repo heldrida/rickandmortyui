b357e7a5f0fe7b5cb535a117e5c409646422d7aa
Helder Oliveira <heldrida@gmail.com>
2021-03-26 20:48:33 +0000

    style: 💄 increase the Pagination text size

--- src/Components/Pagination/index.tsx
+++ src/Components/Pagination/index.tsx
@@ -27,8 +27,8 @@ export const Pagination = ({ filters, page, total, range, }: Pagination) => {
   const display = useDisplayState()
   const routeChange = useRouteChange({})
 
-  const defaultStyle = "align-bottom inline-flex items-center justify-center cursor-pointer leading-5 transition-colors duration-150 font-medium focus:outline-none px-3 py-1 rounded-md text-xs text-gray-600 dark:text-gray-400 focus:outline-none border border-transparent active:bg-transparent hover:bg-gray-100 focus:shadow-outline-gray dark:hover:bg-gray-500 dark:hover:text-gray-300 dark:hover:bg-opacity-10"
-  const activeStyle = "align-bottom inline-flex items-center justify-center cursor-pointer leading-5 transition-colors duration-150 font-medium focus:outline-none px-3 py-1 rounded-md text-xs text-white bg-green-400 border border-transparent active:bg-green-400 hover:bg-green-700 focus:shadow-outline-purple"
+  const defaultStyle = "align-bottom inline-flex items-center justify-center cursor-pointer leading-5 transition-colors duration-150 font-medium focus:outline-none px-3 py-1 rounded-md text-base text-gray-600 dark:text-gray-400 focus:outline-none border border-transparent active:bg-transparent hover:bg-gray-100 focus:shadow-outline-gray dark:hover:bg-gray-500 dark:hover:text-gray-300 dark:hover:bg-opacity-10"
+  const activeStyle = "align-bottom inline-flex items-center justify-center cursor-pointer leading-5 transition-colors duration-150 font-medium focus:outline-none px-3 py-1 rounded-md text-base text-white bg-green-400 border border-transparent active:bg-green-400 hover:bg-green-700 focus:shadow-outline-purple"
 
   const pageHandler = useCallback((idx: number) => {
     ((idx: number) => {
@@ -176,7 +176,7 @@ export const Pagination = ({ filters, page, total, range, }: Pagination) => {
         <ul className="inline-flex items-center">
           <li>
             <button onClick={() => pageHandler(PaginationActions.Back)} className={`align-bottom inline-flex items-center justify-center cursor-pointer leading-5 transition-colors duration-150 font-medium focus:outline-none p-2 rounded-md text-gray-600 dark:text-gray-400 focus:outline-none border border-transparent ${arrowDisableStyleHandler(currentIndex == 1)}`} disabled={currentIndex == 1} type="button" aria-label="Previous">
-              <svg className="h-3 w-3" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+              <svg className="h-6 w-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" fillRule="evenodd"></path>
               </svg>
             </button>
@@ -204,7 +204,7 @@ export const Pagination = ({ filters, page, total, range, }: Pagination) => {
           }
           <li>
             <button onClick={() => pageHandler(PaginationActions.Forward)} className={`align-bottom inline-flex items-center justify-center cursor-pointer leading-5 transition-colors duration-150 font-medium focus:outline-none p-2 rounded-md text-gray-600 dark:text-gray-400 focus:outline-none border border-transparent active:bg-transparent hover:bg-gray-100 focus:shadow-outline-gray dark:hover:bg-gray-500 dark:hover:text-gray-300 dark:hover:bg-opacity-10 ${arrowDisableStyleHandler(currentIndex == total)}`} disabled={currentIndex == total}  type="button" aria-label="Next">
-              <svg className="h-3 w-3" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"><path d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" fillRule="evenodd"></path></svg>
+              <svg className="h-6 w-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"><path d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" fillRule="evenodd"></path></svg>
             </button>
           </li>
         </ul>
